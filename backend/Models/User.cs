190f8f0b3aa2133dccab120adce5fbd514a70a16
s3784464 <s3784464@student.rmit.edu.au>
2021-08-29 22:30:39 +1000

    removed values from User model

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -8,11 +8,6 @@ namespace backend.Models
         public int User_Id { get; set; }
         public string FirstName { get; set; }
         public string Password { get; set; }
-
-        // public User(Guid User_Id, string FirstName, string Password){
-        //     this.User_Id = User_Id;
-        //     this.FirstName = FirstName;
-        //     this.Password = Password;
-        // }
+        
     }
 }
