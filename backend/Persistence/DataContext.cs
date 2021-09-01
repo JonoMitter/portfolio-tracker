c0ace2dcc9394f9726452cb100deb877d1f2e822
s3784464 <s3784464@student.rmit.edu.au>
2021-09-01 21:43:50 +1000

    begun creating SQLite database

---
+++ backend/Persistence/DataContext.cs
@@ -0,0 +1,19 @@
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> user { get; set; }
+        // public DbSet<Holding> holding { get; set; }
+        public DbSet<Cash> cash { get; set; }
+        public DbSet<Crypto> crypto { get; set; }
+        public DbSet<Stock> stock { get; set; }
+        public DbSet<Super> super { get; set; }
+    }
+}
--- backend/Startup.cs
+++ backend/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using backend.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,10 @@ namespace backend
             // {
             //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "backend", Version = "v1" });
             // });
+            services.AddDbContext<DataContext>(opt => 
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
