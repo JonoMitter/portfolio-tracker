abcbcb89c13651957d7d3b50184666cf3fb4ddc0
Richard Gao <49212960+Soupraa@users.noreply.github.com>
2021-09-04 18:57:46 +1000

    user add implemented
    - added user create validation

--- backend/Controllers/UserController.cs
+++ backend/Controllers/UserController.cs
@@ -10,6 +10,9 @@ namespace backend.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+
+        
+
         [HttpGet]
         public List<User> GetAll()
         {
@@ -25,34 +28,45 @@ namespace backend.Controllers
         [HttpPost]
         public IActionResult Create(User user)
         {
-            if(user.Email != null & user.FirstName != null & user.Password !=null){
-                if(Get(user.Email) is null){
-                    UserService.Add(user);
-                }
+            // if(user.Email != null & user.FirstName != null & user.Password !=null){
+            //     if(Get(user.Email) is null){
+            //         UserService.Add(user);
+            //     }
 
-                else{
-                    return BadRequest("Email already in use");
-                }
+            //     else{
+            //         return BadRequest("Email already in use");
+            //     }
+            // }
+
+            // else{
+            //     return BadRequest("Values for Email, FirstName, or Password missing");
+            // }
+            if (ValidateUserCreation(user) is true)
+            {   
+                //adds user to user Service
+                UserService.Add(user);
+                //returns a created status in postman (doesnt add to User service)
+                return CreatedAtAction(nameof(Create), new { id = user.User_Id }, user);
             }
-
-            else{
-                return BadRequest("Values for Email, FirstName, or Password missing");
+            else
+            {
+                return BadRequest("Values for Email, Firstname, password are invalid");
             }
-
-            return CreatedAtAction(nameof(Create), new { id = user.User_Id }, user);
         }
 
         [HttpPut("{email}")]
         public IActionResult Update(string email, User user)
-        {   
+        {
 
-            if( email != user.Email){
+            if (email != user.Email)
+            {
                 return BadRequest();
             }
 
             var existingUser = UserService.Get(email);
 
-            if(existingUser is null){
+            if (existingUser is null)
+            {
                 return NotFound();
             }
 
@@ -63,16 +77,38 @@ namespace backend.Controllers
 
         [HttpDelete("{email}")]
         public IActionResult Delete(string email)
-        {   
+        {
             var user = UserService.Get(email);
 
-            if(user is null){
+            if (user is null)
+            {
                 return NotFound();
             }
 
             UserService.Delete(email);
 
             return NoContent();
+        }
+
+        public Boolean ValidateUserCreation(User user)
+        {
+            //checks for empty values
+            if (user.Email is null || user.Password is null || user.FirstName is null)
+            {
+                return false;
+            }
+            if (!user.Email.Contains("@") & !user.Email.Contains(".com"))
+            {
+                return false;
+            }
+            if (!(Get(user.Email) is null))
+            {
+                return false;
+            }
+            if(user.Password.Length < 4){
+                return false;
+            }
+            return true;
         }
     }
 }
