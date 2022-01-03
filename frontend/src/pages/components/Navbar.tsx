154c629be5ddf75cd9451067ad233283c9fb8afe
Jonathon Mitterbacher <jonomitter@outlook.com>
2022-01-04 00:33:20 +1100

    nav items are centred and user-name is smaller

--- frontend/src/pages/components/Navbar.tsx
+++ frontend/src/pages/components/Navbar.tsx
@@ -3,7 +3,8 @@ import "../styles/Navbar.scss";
 import { Link } from 'react-router-dom';
 import axios from "axios";
 import GetUserResponse from "../../responses/GetUserResponse";
-import { ReactComponent as UserIcon } from "../../assets/user.svg";
+// import { ReactComponent as UserIcon } from "../../assets/user.svg";
+import { ReactComponent as USER_ICON } from "../../assets/person_outline_white_36dp.svg";
 // import LogoutButton from "./LogoutButton";
 
 const Navbar = (props: { user: GetUserResponse, setUser: (user: GetUserResponse) => void }) => {
@@ -37,7 +38,7 @@ const Navbar = (props: { user: GetUserResponse, setUser: (user: GetUserResponse)
   } else {
     menu = (
       <ul className="nav-right">
-        <li><div className="nav-item nav-name"> <UserIcon className="white-svg" /> {props.user.firstName}</div></li>
+        <li><div className="nav-item nav-user"> <USER_ICON className="white-svg" /> <span className="nav-name">{props.user.firstName}</span></div></li>
         <li><Link className="nav-item logout-button scale-hover" to='/' onClick={logout}>Logout</Link></li>
 
         {/* <li><LogoutButton /></li> */}
